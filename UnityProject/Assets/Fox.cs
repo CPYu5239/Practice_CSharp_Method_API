d323a3a06f8ab6f8c3fc8e4e5fa1e977d2013ee8
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-11-22 21:28:35 +0800

    實作鍵盤控制角色移動，並在移動時做圖片換邊，能夠推動箱子

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Tooltip("角色移動速度")]
+    [Range(0.1f, 99.9f)]
+    public float speed = 1f;
+
+    public Rigidbody2D rig;
+    public SpriteRenderer psr;
+
+    private void FoxMove()
+    { 
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Vector3 vecR = new Vector3(speed * Time.deltaTime, 0, 0);
+            psr.transform.Translate(vecR);
+            psr.flipX = false;
+            rig.AddForce(new Vector2(speed * Time.deltaTime, 0));
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Vector3 vecL = new Vector3(-speed * Time.deltaTime, 0, 0);
+            psr.transform.Translate(vecL);
+            psr.flipX = true;
+            rig.AddForce(new Vector2(-speed * Time.deltaTime, 0));
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        FoxMove();
+    }
+}
